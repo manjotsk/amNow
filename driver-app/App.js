521a27efd455fd292824653a11759737c5b25a77
Ansh Gujral <anshgujral1997@gmail.com>
2018-11-18 12:39:31 +0530

    added functionality to ignore warning
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- driver-app/App.js
+++ driver-app/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 
 import Routes from './src/Routes'
 import store from './src/store/store'
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+    console.disableYellowBox = true;
+  }
+
   render() {
     return (
       <Provider store={store}>
